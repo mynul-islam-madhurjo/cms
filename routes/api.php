113a1cb79fdc4560ad882875250d37e528cd01bb
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-05-21 17:41:38 +0600

    jwt token

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,32 @@
 <?php
 
-Route::ApiResource('/list',\App\Http\Controllers\UserController::class)
+
+
+
+//Route::apiResource('/list', App\Http\Controllers\UserController::class);
+//
+//Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+//
+//
+//Route::group(['middleware' => 'auth:api'], function () {
+//
+//    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
+//    Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+//    Route::post('me', [App\Http\Controllers\AuthController::class, 'me']);
+//});
+
+
+
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+
+//    Route::post('login', 'AuthController@login');
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+//    Route::post('logout', 'AuthController@logout');
+//    Route::post('refresh', 'AuthController@refresh');
+//    Route::post('me', 'AuthController@me');
+
+});
+
 
 
 ?>
